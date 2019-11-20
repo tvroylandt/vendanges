a4d00953d09055eacde83768c44223df8cffcf7a
Thomas Vroylandt <tvroylandt@gmail.com>
2019-11-20 09:20:53 +0100

    Import DADS

--- R/clean_dads.R
+++ R/clean_dads.R
@@ -1,39 +1,49 @@
 # ------------------------------------- #
-# Vendanges - DADS #
+# Vendanges - Nettoyage DADS #
 # ------------------------------------- #
 
 library(foreign)
 library(tidyverse)
-library(writexl)
-
-# Variables nécessaires
-# SEXE
-# AGE_TR (ou AGE)
-# PCS
-# POND
-# DATDEB
-# DATFIN
-# DEP
-# ETRANGER
-
 
 # Disponibilité des variables ---------------------------------------------
-# On ne conserve que les variables présentes sur toute la période
-varlist_ok <- tibble(annee = seq(2005, 2015, 1),
-                    fichier = paste0("data/dads_source/varlist_postes", str_sub(annee, 3, 4), ".dbf"),
-                    varlist = map(fichier, read.dbf)) %>% 
-  unnest(varlist) %>% 
-  group_by(VARIABLE) %>% 
-  count() %>% 
-  filter(n == 11)
+varlist_ok <- tibble(
+  annee = seq(2005, 2015, 1),
+  fichier = paste0(
+    "data/dads_source/varlist_postes",
+    str_sub(annee, 3, 4),
+    ".dbf"
+  ),
+  varlist = map(fichier, read.dbf)
+) %>%
+  unnest(varlist) %>%
+  group_by(VARIABLE) %>%
+  count()
 
-# Chargement --------------------------------------------------------------
-dads2015_postes <- read.dbf("Data/dads2015_postes/postes15.dbf") %>% 
-  as_tibble()
+# Import DADS -------------------------------------------------------------
+dads_postes <- tibble(
+  annee = seq(2005, 2015, 1),
+  fichier = paste0("data/dads_source/postes", str_sub(annee, 3, 4), ".dbf"),
+  postes = map(fichier, function(x) {
+    read.dbf(x) %>%
+      as_tibble() %>%
+      filter(PCS == "691D")
+  })
+) %>%
+  select(annee, postes) %>% 
+  unnest(cols = postes)
 
-dads2010_postes <- read.dbf("Data/dads2010_postes/postes10.dbf") %>% 
-  as_tibble()
 
+# Export ------------------------------------------------------------------
+write_rds(dads_postes, "data/dads_filtre_05_15.rds")
+
+
+
+
+# ----------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
+# ----------------------------------------------------------------------- #
 
 # Démographie -------------------------------------------------------------
 # 2015
